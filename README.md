191003d884ab816bd006b544abdf03408b80172c
GGu <lei.gu@mybogames.com>
2023-05-12 12:29:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# clash-setting
+
+## 执行参数说明
+```text
+  -dir string
+        配置文件地址 📁 , 默认配置文件 template.yaml & proxy-filters.yaml 文件夹 (default "./")
+  -file string
+        导出配置文件名称 📃 (default "config.yaml")
+  -out string
+        导出文件夹 📁 (default "out/")
+  -urls string
+        订阅链接地址 🔗 (多个订阅链接之间 "," 分隔)
+```
+
+
+## template.yaml 自定义配置模版
+
+预先定义用户配置文件模版（除 `proxy-providers` 和 `proxy-groups`
+的其他配置），脚本会根据订阅地址和规则生成填充 `proxy-providers`和 `proxy-groups` 配置。`rule-provider`、`rules`...相关配置自行在
+template.yaml 中配置。
+
+## configuration.yaml 脚本配置
+
+* `filters`: `proxy-providers` 中根据 `filter` 生成的节点分组，机场间单独分开（例如：`name: jp-airport1`
+  ），后面会填充到对应的 `use_filters` 中,
+  对于 `filter` 正则匹配不存在节点的分组则会自动跳过。 `interval` 为刷新间隔。
+
+```yaml
+filters:
+  - { name: "hk", filter: '香港', interval: 86400 }
+  - { name: "tw", filter: '台湾' }
+  - { name: "jp", filter: '日本' }
+```
+
+* `groups`: `group` 为 `GroupCommonOption`
+  的相关配置（详见：https://github.com/Dreamacro/clash/blob/7f1b50f4a70fe353b8ea5d31ecfd4c751b3f2eb0/adapter/outboundgroup/parser.go#L23,
+  ）, `use_filters` 为所使用的 `filters` 中分组名称，`rm_airports` 表示是否移除 `proxies` 中的机场节点（默认会添加所有机场分组）。
+
+```yaml
+groups:
+  - { group: { name: "Apple", type: select, proxies: [ "DIRECT", "Proxy" ] }, use_filters: [ "us","jp" ] }
+  - { group: { name: "Amazon", type: select, proxies: [ "Proxy" ] }, use_filters: [ "us","sg","hk" ] }
+  - { group: { name: "YouTube", type: url-test, proxies: [ "Proxy" ], url: 'http://www.gstatic.com/generate_204', interval: 300 }, rm_airports: true }
+```
---
+++ main.go
@@ -0,0 +1,359 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/Dreamacro/clash/component/dialer"
+	"github.com/mitchellh/mapstructure"
+	"github.com/samber/lo"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	templateFile      = "template.yaml"
+	configurationFile = "configuration.yaml"
+
+	timeout  = 30 * time.Second
+	interval = 86400
+)
+
+var (
+	fileMode os.FileMode = 0o666
+	dirMode  os.FileMode = 0o755
+
+	urls, dir, file, out string
+
+	urlList     []string
+	configMap   = make(map[string]any)
+	proxyConfig ProxyConfig
+
+	defaultHealthCheck = map[string]any{
+		"enable":   true,
+		"url":      "http://www.gstatic.com/generate_204",
+		"interval": 3600,
+	}
+)
+
+type ProxyConfig struct {
+	Filters []Filters `yaml:"filters"`
+	Groups  []struct {
+		Group      Group    `yaml:"group"`
+		UseFilters []string `yaml:"use_filters"`
+		RmAirports bool     `yaml:"rm_airports"`
+	} `yaml:"groups"`
+}
+
+type Filters struct {
+	Name   string `yaml:"name"`
+	Filter string `yaml:"filter"`
+
+	provider []string `yaml:"-"`
+}
+
+type Group struct {
+	Name       string   `yaml:"name" mapstructure:"name"`
+	Type       string   `yaml:"type" mapstructure:"type"`
+	Proxies    []string `yaml:"proxies" mapstructure:"proxies,omitempty"`
+	Use        []string `yaml:"use" mapstructure:"use,omitempty"`
+	URL        string   `yaml:"url" mapstructure:"url,omitempty"`
+	Interval   int      `yaml:"interval" mapstructure:"interval,omitempty"`
+	Lazy       bool     `yaml:"lazy" mapstructure:"lazy,omitempty"`
+	DisableUDP bool     `yaml:"disable-udp" mapstructure:"disableUDP,omitempty"`
+	Filter     string   `yaml:"filter" mapstructure:"filter,omitempty"`
+}
+
+type ProxySchema struct {
+	Proxies []map[string]any `yaml:"proxies"`
+}
+
+func init() {
+	flag.StringVar(&urls, "urls", "", "订阅链接地址 🔗 (多个订阅链接之间 \",\" 分隔)")
+	flag.StringVar(&dir, "dir", "./", "配置文件地址 📁 , 默认配置文件 template.yaml & proxy-filters.yaml 文件夹")
+	flag.StringVar(&file, "file", "config.yaml", "导出配置文件名称 📃")
+	flag.StringVar(&out, "out", "out/", "导出文件夹 📁")
+	flag.Parse()
+
+	if strings.TrimSpace(urls) == "" {
+		log.Fatal("urls is empty")
+	}
+	urlList = strings.Split(urls, ",")
+}
+
+func main() {
+	// loading template.yaml
+	if err := unmarshal(filepath.Join(dir, templateFile), &configMap); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// loading proxy-filters.yaml
+	if err := unmarshal(filepath.Join(dir, configurationFile), &proxyConfig); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	airportFiles := make(map[string]map[string]any)
+	// download proxy file && add proxy providers
+	if _, ok := configMap["proxy-providers"]; !ok {
+		configMap["proxy-providers"] = make(map[string]any)
+	}
+	for _, v := range urlList {
+		filename, proxySchema, err := downloadProxies(v)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		// filename 配置文件默认同级文件夹
+
+		provider := map[string]any{
+			"type":         "http",
+			"path":         filename,
+			"url":          v,
+			"interval":     interval,
+			"health-check": defaultHealthCheck,
+		}
+
+		name := path.Base(filename[:len(filename)-len(path.Ext(filename))])
+		airportFiles[name] = map[string]any{
+			"path":        filename,
+			"proxySchema": proxySchema,
+		}
+
+		configMap["proxy-providers"].(map[string]any)[name] = provider
+	}
+
+	proxyFilterMap := make(map[string]Filters)
+
+	// add filter proxy providers
+	for _, v := range proxyConfig.Filters {
+		filterReg, err := regexp.Compile(v.Filter)
+		if err != nil {
+			log.Fatalf("invalid filter regex: %w", err)
+			return
+		}
+
+		for name, m := range airportFiles {
+			// NOTICE: 不存在 filter 条件的节点过滤不添加
+			proxySchema := m["proxySchema"].(*ProxySchema)
+			exists := lo.ContainsBy(proxySchema.Proxies, func(item map[string]any) bool {
+				return filterReg.MatchString(item["name"].(string))
+			})
+
+			// 存在匹配到规则的 proxy
+			if exists {
+				// providerName: jp-sub.xsus.me
+				providerName := fmt.Sprintf("%s-%s", v.Name, name)
+				v.provider = append(v.provider, providerName)
+				proxyFilterMap[v.Name] = v
+
+				provider := map[string]any{
+					"type":         "file",
+					"path":         m["path"],
+					"filter":       v.Filter,
+					"interval":     interval,
+					"health-check": defaultHealthCheck,
+				}
+				configMap["proxy-providers"].(map[string]any)[providerName] = provider
+			}
+		}
+	}
+
+	// add proxy groups
+	if _, ok := configMap["proxy-groups"]; !ok {
+		configMap["proxy-groups"] = []map[string]any{}
+	}
+	proxyGroups := configMap["proxy-groups"].([]map[string]any)
+	// add default proxy groups
+	proxyGroups = append(
+		proxyGroups,
+		map[string]any{
+			"name": "Proxy",
+			"type": "select",
+			"use":  lo.Keys(airportFiles),
+		},
+	)
+	// add airport proxy groups
+	for name := range airportFiles {
+		proxyGroups = append(
+			proxyGroups,
+			map[string]any{
+				"name": name,
+				"type": "select",
+				"use":  []string{name},
+			},
+		)
+	}
+	// add custom proxy groups
+	for _, v := range proxyConfig.Groups {
+		// proxy 里添加机场
+		if !v.RmAirports {
+			v.Group.Proxies = append(v.Group.Proxies, lo.Keys(airportFiles)...)
+		}
+
+		// 替换相对应的 use_filters, us => us-sub.xsus.me, 不存在节点的自动跳过
+		if len(v.UseFilters) > 0 {
+			for _, filterName := range v.UseFilters {
+				if _, exists := proxyFilterMap[filterName]; exists {
+					v.Group.Use = append(v.Group.Use, proxyFilterMap[filterName].provider...)
+				}
+			}
+		}
+
+		var group map[string]any
+		err := mapstructure.Decode(v.Group, &group)
+		if err != nil {
+			log.Fatalf("mapstructure.Decode(%s) err: %v", v.Group.Name, err)
+			return
+		}
+
+		proxyGroups = append(proxyGroups, group)
+	}
+
+	proxyGroups = append(proxyGroups, map[string]any{
+		"name":    "MATCH",
+		"type":    "select",
+		"proxies": []string{"Proxy"},
+		"use":     lo.Keys(airportFiles),
+	})
+	configMap["proxy-groups"] = proxyGroups
+
+	buf, err := yaml.Marshal(configMap)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = safeWrite(filepath.Join(out, file), buf)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	log.Println("finish")
+}
+
+func downloadProxies(rawURL string) (string, *ProxySchema, error) {
+	uri, err := url.Parse(rawURL)
+	if err != nil {
+		return "", nil, err
+	}
+
+	buf, err := request(uri)
+	if err != nil {
+		return "", nil, err
+	}
+
+	schema := &ProxySchema{}
+	if err := yaml.Unmarshal(buf, schema); err != nil {
+		return "", nil, err
+	}
+
+	if schema.Proxies == nil {
+		return "", nil, fmt.Errorf("file must have a `proxies` field (%s)", rawURL)
+	}
+
+	filename := fmt.Sprintf("%s.yaml", uri.Host)
+	err = safeWrite(filepath.Join(out, filename), buf)
+	if err != nil {
+		return "", nil, fmt.Errorf("file to write %s (%s)", filepath.Join(out, filename), rawURL)
+	}
+
+	return filename, schema, nil
+}
+
+func request(uri *url.URL) ([]byte, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	req, err := http.NewRequest(http.MethodGet, uri.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if user := uri.User; user != nil {
+		password, _ := user.Password()
+		req.SetBasicAuth(user.Username(), password)
+	}
+
+	req = req.WithContext(ctx)
+
+	transport := &http.Transport{
+		// from http.DefaultTransport
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		DialContext: func(ctx context.Context, network, address string) (net.Conn, error) {
+			return dialer.DialContext(ctx, network, address)
+		},
+	}
+
+	client := http.Client{Transport: transport}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	buf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
+
+func safeWrite(path string, buf []byte) error {
+	dir := filepath.Dir(path)
+
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, dirMode); err != nil {
+			return err
+		}
+	}
+
+	return os.WriteFile(path, buf, fileMode)
+}
+
+func unmarshal(path string, val interface{}) error {
+	buf, err := readConfig(path)
+	if err != nil {
+		return fmt.Errorf("fail to readConfig %s: %v", path, err)
+	}
+
+	if err := yaml.Unmarshal(buf, val); err != nil {
+		return fmt.Errorf("fail to yaml.Unmarshal %s: %v", path, err)
+	}
+
+	return nil
+}
+
+func readConfig(path string) ([]byte, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return nil, err
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		return nil, fmt.Errorf("configuration file %s is empty", path)
+	}
+
+	return data, err
+}
